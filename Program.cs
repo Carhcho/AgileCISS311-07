546f2fef895ddb043152e51fd59d939e4806a26f
Carhcho <63563365+Carhcho@users.noreply.github.com>
2023-08-21 22:41:27 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dropbox07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Alice", 3.4);
+            Student s2 = new Student("Bob", 3.4);
+            Student s3 = new Student("Chuck", 3.4);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            s1.GPA = 3.5;
+            s2.GPA = 3.4;
+            s3.GPA = 3.3;
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dropbox07
+{
+    public delegate void UpdateGpaDelegate();
+    public delegate void UpdateGpaDelegate2();
+    class Student
+    {
+        private string name;
+        private double gpa;
+        public event UpdateGpaDelegate UpdateGpaEvent;
+        public event UpdateGpaDelegate2 UpdateGpaEvent2;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double GPA
+        {
+            get { return gpa; }
+            set
+            {
+               if(value == gpa)
+                {
+                    gpa = value;
+                }
+                    else
+                    if (value > gpa)
+                    {
+                    UpdateGpaEvent += onIncrease;
+                    gpa = value;
+                        UpdateGpaEvent();
+                    }
+                    
+                    else
+                    if (value < gpa)
+                    {
+                    UpdateGpaEvent2 += onDecrease;
+                    gpa = value;
+                        UpdateGpaEvent2();
+                    }
+               
+            }
+        }
+        public Student (string name, double gpa)
+        {
+            this.name = name;
+            this.gpa = gpa;
+        }
+        public void onIncrease()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        public void onDecrease()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        public override string ToString()
+        {
+            string str;
+            str = string.Format($"Name: {name} GPA: {gpa}");
+            return str;
+        }
+    }
+}
